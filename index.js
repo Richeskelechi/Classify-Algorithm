97aa6fcdf829f3e378766342cb411ed6fd687fdf
Riches Kelechi <ohanu.riches@ictgiglogistics.onmicrosoft.com>
2023-10-02 10:47:27 +0100

    Basic Algorithm Template

---
+++ index.js
@@ -0,0 +1,11 @@
+// Import The Student Data
+import studentData from './data.js';
+
+// Now you can use studentData as an array of objects
+
+function classify(student){
+    // Do Your Solution Inside Here...
+}
+
+console.log(classify(studentData));
+
