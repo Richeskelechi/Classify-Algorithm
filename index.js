c6f5792da609dc861d8d68e046b1b320cd309bcc
Riches Kelechi <ohanu.riches@ictgiglogistics.onmicrosoft.com>
2023-10-02 11:07:20 +0100

    Modified The data.js a little

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import studentData from './data.js';
 
 // Now you can use studentData as an array of objects
 
-function classify(student){
+function classify(student) {
     // Do Your Solution Inside Here...
+    
 }
 
 console.log(classify(studentData));
